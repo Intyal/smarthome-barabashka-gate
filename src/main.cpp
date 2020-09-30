f15d7ac5a22242d6c4a1324c436fe1863ca417dc
Барабошкин Сергей Вячеславович <sv.baraboshkin.66@fss.local>
2020-09-30 17:30:05 +0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-// Библиотека Ардуино
 #include "Arduino.h"
-
 #include "IPAddress.h"
 #include "painlessMesh.h"
-
-#include <AsyncTCP.h>
-
-#include <ESPAsyncWebServer.h>
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
 
 // Класс работы с файлом канфигурации
 #include "config.h"
@@ -17,77 +13,104 @@
 
 int LED = 22;
 
+Scheduler deviceScheduler; // Управление задачами
+
+painlessMesh mesh; // Объект Mesh сети
+AsyncWebServer server(80); // Объект Web сервера
+IPAddress myIP(0,0,0,0); // IP адрес устройства
+IPAddress myAPIP(0,0,0,0); // IP адрес точки доступа
+
 // Prototype
 void receivedCallback( const uint32_t &from, const String &msg );
 IPAddress getlocalIP();
 
-Scheduler     userScheduler; // to control your personal tas
 
-painlessMesh  mesh;
-AsyncWebServer server(80);
-IPAddress myIP(0,0,0,0);
-IPAddress myAPIP(0,0,0,0);
-
-// Зажигаем или гасим светодиод
+// Задача по миганию светодиодом
 Task BuildLedBlink(1000, TASK_FOREVER, []() {
 	digitalWrite(LED, !digitalRead(LED));
 });
 
 void setup() {
+	// Инициализация серийного порта
 	Serial.begin(115200);
 
-	pinMode (LED, OUTPUT);
+	// Выходной Пин светодиода
+	pinMode(LED, OUTPUT);
 
+	// Настройки
+	//
+	// Объект настроек устройства
 	Config gate_config(FILE_CONFIG);
+	// Данные с настройками устройства
 	config_data_t config_data;
-
+	// Вывод настроек в серийный порт
 	gate_config.printConfig();
+	// Загрузка настроек
 	gate_config.loadConfig(config_data);
 
-	mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION | MESH_STATUS | APPLICATION );  // set before init() so that you can see startup messages
-
-	// Channel set to 6. Make sure to use the same channel for your mesh and for you other
-	// network (STATION_SSID)
+	// Mesh сеть
+	//
+	// Установка вывода сообщений в серийный порт
+	mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION | MESH_STATUS | APPLICATION );
+	// Инициализация MESH сети на канале 6. Режим точки доступа WiFi при старте устройства.
 	mesh.init( config_data.mesh_prefix, config_data.mesh_password, config_data.mesh_port, WIFI_AP_STA, 6 );
+	// Установка процедуры обратного вызова для сообщений, адресованных этому узлу
 	mesh.onReceive(&receivedCallback);
-
+	// Подключение к WiFi сети
 	mesh.stationManual(config_data.wifi_ssid, config_data.wifi_password);
+	// Установка имени узла
 	mesh.setHostname(HOSTNAME);
-
-	// Bridge node, should (in most cases) be a root node. See [the wiki](https://gitlab.com/painlessMesh/painlessMesh/wikis/Possible-challenges-in-mesh-formation) for some background
+	// Устанавливаеи узел как корневой
 	mesh.setRoot(true);
-	// This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
+	// Сообщать всем узлам что этот узел является корневым
 	mesh.setContainsRoot(true);
 
+	// Wifi сеть
+	//
+	// Вывод IP ареса точки достпа
 	myAPIP = IPAddress(mesh.getAPIP());
 	Serial.println("My AP IP is " + myAPIP.toString());
 
-	//Async webserver
-	//server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-	//	request->send(SPIFFS, "/index.html");
-	//});
+	// Web сервер
+	//
+	// Асинхронная обработка запросов
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+		const size_t capacity = 8 * 64;
+		StaticJsonDocument<capacity> cfg;
+		JsonObject root  = cfg.to<JsonObject>();
+		root["entity_id"] = "test entity";
+		char json_string[256];
+		serializeJson(root, json_string);
+        request->send(200, "text/plain", json_string);
+    });
+	// Статическая выдача страниц
 	server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-
+	// Старт Web сервера
 	server.begin();
 
-	userScheduler.addTask(BuildLedBlink);
+	// Добавление задач в планировщик
+	//
+	// Светодиод
+	deviceScheduler.addTask(BuildLedBlink);
 	BuildLedBlink.enable();
 	
 }
  
 void loop() {
+	// Выполнение задач из планировщика объекта painlessMesh mesh
 	mesh.update();
-	if(myIP != getlocalIP()){
-		myIP = getlocalIP();
+
+	// Проверка текущего IP устройства
+	if(myIP != IPAddress(mesh.getStationIP())){
+		myIP = IPAddress(mesh.getStationIP());
 		Serial.println("My IP is " + myIP.toString());
 	}
-	userScheduler.execute();
+
+	// Выполнение задач из планировщика устройства
+	deviceScheduler.execute();
 }
 
+// Процедура обратного вызова для сообщений, адресованных этому узлу
 void receivedCallback( const uint32_t &from, const String &msg ) {
   Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
-}
-
-IPAddress getlocalIP() {
-  return IPAddress(mesh.getStationIP());
 }
